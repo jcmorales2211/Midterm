2737ce353c07c219d8ca938bfb6cb6f1452a7b2a
jcmorales2211 <jcmorales2211@gmail.com>
2018-08-23 15:28:37 +0800

    SW-Midterm<Morales, Jeremiah C.>

--- SW-Midterm-Morales, Jeremiah C.R
+++ SW-Midterm-Morales, Jeremiah C.R
@@ -5,30 +5,27 @@ biocLite("EBImage")
 library(EBImage)
 library(jpeg)
 library(magick)
-#local read
+
+#WEB Read and resize
+hoop<- image_read('https://www.pump.org/wp-content/uploads/2018/04/FT9FPNZHVR7USAM.LARGE_.jpg')
+print(hoop) 
+image_scale(hoop, "250")
+
+#local resize
 ball<- readJPEG("bball.jpg", native = TRUE)
 ball
 plot(0:1,0:1, type = "n", ann= FALSE, axes = FALSE)
 rasterImage(pict,0,0,1,1)
 
-#WEB Read
-hoop<- image_read('https://www.pump.org/wp-content/uploads/2018/04/FT9FPNZHVR7USAM.LARGE_.jpg')
-print(hoop) 
-
 ball<- readImage("bball.jpg")
-hoop<-readImage("hoop.jpg")
 dim(ball)[1:2]
-dim(hoop)[1:2]
+
 #specific resizing
 ballImage <- resize(ball, w = 200, h = 100)
-hoopImage <- resize(hoop, w = 200, h = 100)
-
 # scale by 50%; the height is determined automatically so that
 # the aspect ratio is preserved
 ballImage <- resize(bballImage, dim(bballImage)[1]/2)
-hoopImage <- resize(bballImage, dim(bballImage)[1]/2)
 # show the scaled image
-display(hoopImage)
 display(ballImage)
 
 
